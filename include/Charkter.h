4717e323315f3918c68b5de873ed1ea2f8d5b7da
if23b191 <if23b191@technikum-wien.at>
2024-02-23 00:08:42 +0100

    added the charekter class

---
+++ include/Charkter.h
@@ -0,0 +1,20 @@
+#ifndef CHARKTER_H
+#define CHARKTER_H
+
+
+class Charkter
+{
+    public:
+        Charkter()
+        ~Charkter();
+        int x;
+        int y;
+        int lifePoints;
+        int relicPointValue;
+
+    protected:
+
+    private:
+};
+
+#endif // CHARKTER_H
---
+++ src/Charkter.cpp
@@ -0,0 +1,14 @@
+#include "Charkter.h"
+
+Charkter::Charkter()
+{
+    this->x = 0;
+    this->y = 0;
+    this->lifePoints = 5;
+    this->relicPointValue =0;
+}
+
+Charkter::~Charkter()
+{
+    //dtor
+}
